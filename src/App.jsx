836e5bc37e7cb0e4d2aaa925de726b52db016d39
bazzab12 <barry@thegreyhill.com>
2026-03-13 10:25:44 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GreyHillWidget from './GreyHillWidget';
+
+// This is the entry point for the embed widget
+function App() {
+  return (
+    <React.StrictMode>
+      <GreyHillWidget />
+    </React.StrictMode>
+  );
+}
+
